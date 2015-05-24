607e7589f577dd672aa4e9c9e7580fa3a8e3c016
Osservatorio Astronimico di Seveso <astrosev@gmail.com>
2015-05-24 17:13:35 +0200

    JUNK!
     Changes to be committed:
    new file:   CMakeCache.txt
    	new file:   CMakeFiles/3.0.2/CMakeCCompiler.cmake

---
+++ CMakeFiles/ConsoleSbig.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/algebrato/ConsoleSbig/src/ConsoleSbig.cpp" "/home/algebrato/ConsoleSbig/CMakeFiles/ConsoleSbig.dir/src/ConsoleSbig.cpp.o"
+  "/home/algebrato/ConsoleSbig/src/csbigcam.cpp" "/home/algebrato/ConsoleSbig/CMakeFiles/ConsoleSbig.dir/src/csbigcam.cpp.o"
+  "/home/algebrato/ConsoleSbig/src/csbigimg.cpp" "/home/algebrato/ConsoleSbig/CMakeFiles/ConsoleSbig.dir/src/csbigimg.cpp.o"
+  "/home/algebrato/ConsoleSbig/src/getImage.cpp" "/home/algebrato/ConsoleSbig/CMakeFiles/ConsoleSbig.dir/src/getImage.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- src/getImage.cpp
+++ src/getImage.cpp
@@ -19,6 +19,7 @@ void *checkTemp(void *cam){
     pthread_mutex_lock(&mutex);
 
     int *ret;
+    ret= new int();
     CSBIGCam *camera = (CSBIGCam *)cam;
     double ccdTemp = (double)rand()/((double)RAND_MAX);
     double percentTE = (double)rand()/((double)RAND_MAX);
@@ -26,6 +27,10 @@ void *checkTemp(void *cam){
     double endpointTemp;
     int termalized=1;
     int count=0;
+    int counter=0;
+    double delta=3.00;
+    bool reverse;
+    string yes_grab;
 
     MY_LOGICAL enabled=true;
     MY_LOGICAL isenable;
@@ -34,25 +39,57 @@ void *checkTemp(void *cam){
     cin >> setpointTemp;
     cout << "Set end point temperature   = ";
     cin >> endpointTemp;
+    if(setpointTemp > endpointTemp){
+	    reverse = false;
+    }else{
+	    reverse = true;
+    }
 
     pthread_cond_wait(&cond,&mutex);
 
     while(true){
-        //camera->SetTemperatureRegulation(enabled, setpointTemp);
-        //camera->QueryTemperatureStatus(isenable, ccdTemp, setpointTemp, percentTE);
+        camera->SetTemperatureRegulation(enabled, setpointTemp);
+        camera->QueryTemperatureStatus(isenable, ccdTemp, setpointTemp, percentTE);
+	setpointTemp-=0.19;
+	camera->SetTemperatureRegulation(enabled, setpointTemp);
         printf("\n\033[F\033[J");
-        ccdTemp = (double)rand()/((double)RAND_MAX);
-        percentTE = (double)rand()/((double)RAND_MAX);
         cout << "Temp control: CCDTemp=" << ccdTemp << " STP="<<setpointTemp << " Power=" <<percentTE*100 << "%";
 
-        if(count%30==0 && setpointTemp > endpointTemp){
-            setpointTemp -= 3.00;
-            if(setpointTemp<endpointTemp) setpointTemp = endpointTemp;
+        if(count%4==0 && setpointTemp > endpointTemp && reverse==false){
+            setpointTemp -= delta;
+	    if(setpointTemp < endpointTemp) setpointTemp = endpointTemp;
         }
 
-        if(termalized==1 && abs(endpointTemp-ccdTemp) < 0.1){
-            cout << endl << "Termalized!!" << endl ;
-            termalized=0;
+	if(count%4==0 && setpointTemp < endpointTemp && reverse==true){
+            setpointTemp += delta;
+	    if(setpointTemp > endpointTemp) setpointTemp = endpointTemp;
+	}
+
+        if(termalized==1 && abs(endpointTemp-ccdTemp) < 0.4){
+            cout << endl << "Start termalizzation..." << endl;
+
+
+            while(true){
+		    ++counter;
+		    camera->QueryTemperatureStatus(isenable, ccdTemp, setpointTemp, percentTE);
+		    printf("\n\033[F\033[J");
+		    cout << "Temp control: CCDTemp=" << ccdTemp << " STP="<<setpointTemp << " Power=" <<percentTE*100 << "%" << " Iterator = (" << counter <<"/35)";
+		    if(counter%35==0){
+			    cout <<endl;
+			    cout << "Start image grab? (yes/no): ";
+			    cin >> yes_grab;
+			    if(yes_grab=="yes"){
+				 break;
+			    }else{
+				    counter=0;
+			    }
+		    }
+		    sleep(1);
+	    }
+	    
+	    
+	    cout << endl << "Termalized!" << endl;
+	    termalized=0;
             pthread_cond_signal(&cond2);
             pthread_mutex_unlock(&mutex);
         }
@@ -70,7 +107,10 @@ void *grabImage(void *cam){
 
     pthread_mutex_lock(&mutex);
 
-    int *ret;
+    int *rit;
+    rit= new int();
+    rit[1]=1;
+    cout << "rit = " << rit[1] << endl;
     int num_img;
     string name_img;
     string path_save("/home/algebrato/");
@@ -93,5 +133,7 @@ void *grabImage(void *cam){
         sleep(5);
         cout << endl << "Image "<< i+1 << " grabbed" << endl;
     }
-    pthread_exit(ret);
+    pthread_exit(rit);
+    cout << "rit = " << rit[1] << endl;
+    return NULL;
 }
